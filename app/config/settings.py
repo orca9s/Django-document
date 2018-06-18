2b3234a26b47e045bec30331711c2a7b5f262c58
sangmin <sang93423@gmail.com>
2018-06-18 11:43:25 +0900

    ForeignKey

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'models.foreignkey.apps.ForeignkeyConfig'
+    'models.foreignkey.apps.ForeignkeyConfig',
     'models.fields.apps.FieldsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- app/models/foreignkey/models.py
+++ app/models/foreignkey/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 
 
-class Manufacturer(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-    
 class Car(models.Model):
     manufacturer = models.ForeignKey(
-        Manufacturer,
+        # 문자열로 바꿔주면 car클래스를 위로 써도 된다. 파이썬에서는 안되고 장고에서 제공하는 기능이다.
+        'Manufacturer',
         on_delete=models.CASCADE,
     )
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    instructor = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        related_name='students',
+        blank=True,
+        null=True,
+    )
